4a32c3bdd53e0808336e392a7e8017edd614e372
Jim.Dev <141078339+JimChDev@users.noreply.github.com>
2025-03-01 17:34:30 -0500

    Create member.js
    Copilot second commit"

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillMember() {
+    var skillMember = document.getElementById("skillMember");
+    var skillMemberValue = skillMember.value;
+    var skillMemberValue = parseInt(skillMemberValue);
+    if (skillMemberValue < 0 || skillMemberValue > 100) {
+        alert("请输入0-100之间的数字");
+        skillMember.value = "";
+    }
+}
